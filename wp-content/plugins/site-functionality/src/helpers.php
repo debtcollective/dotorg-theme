485eb07fe2e3e48bc353b29cd3a8dc2995198cd3
Pea <pea@misfist.com>
2021-08-15 20:57:08 -0400

    Add function to get timezone from long/lat

--- wp-content/plugins/site-functionality/src/helpers.php
+++ wp-content/plugins/site-functionality/src/helpers.php
@@ -14,6 +14,53 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Get timezone based on longitude and latitude
+ *
+ * @param string $cur_lat
+ * @param string $cur_long
+ * @param string $country_code
+ * @return mixed timezone || 'unknown'
+ */
+function get_nearest_timezone( $cur_lat, $cur_long, $country_code = '' ) {
+    $timezone_ids = ( $country_code ) ? \DateTimeZone::listIdentifiers( \DateTimeZone::PER_COUNTRY, $country_code )
+                                    : \DateTimeZone::listIdentifiers();
+
+    if( $timezone_ids && is_array( $timezone_ids ) && isset( $timezone_ids[0] ) ) {
+
+        $time_zone = '';
+        $tz_distance = 0;
+
+        //only one identifier?
+        if ( count($timezone_ids) == 1 ) {
+            $time_zone = $timezone_ids[0];
+        } else {
+
+            foreach( $timezone_ids as $timezone_id ) {
+                $timezone = new \DateTimeZone( $timezone_id );
+                $location = $timezone->getLocation();
+                $tz_lat   = $location['latitude'];
+                $tz_long  = $location['longitude'];
+
+                $theta    = $cur_long - $tz_long;
+                $distance = ( sin( deg2rad( $cur_lat ) ) * sin( deg2rad( $tz_lat ) ) ) 
+                + ( cos( deg2rad( $cur_lat ) ) * cos( deg2rad( $tz_lat ) ) * cos( deg2rad( $theta ) ) );
+                $distance = acos( $distance );
+                $distance = abs( rad2deg( $distance ) );
+                // echo '<br />'.$timezone_id.' '.$distance; 
+
+                if ( !$time_zone || $tz_distance > $distance ) {
+                    $time_zone   = $timezone_id;
+                    $tz_distance = $distance;
+                } 
+
+            }
+        }
+        return  $time_zone;
+    }
+    return 'unknown';
+}
+
+/**
  * Debug Helper
  */
 if( !function_exists( 'console_log' ) ) {
