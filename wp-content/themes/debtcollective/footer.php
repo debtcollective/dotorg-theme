cfa473d973d06dcbaae9916b064b29f98458d0d1
Pea <pea@misfist.com>
2021-09-02 16:58:51 -0400

    Add menu and widget area
    Add privacy menu location
    Render footer widget area

--- wp-content/themes/debtcollective/footer.php
+++ wp-content/themes/debtcollective/footer.php
@@ -12,6 +12,17 @@
 ?>
 
 	<footer class="site-footer">
+		<?php debtcollective_display_social_network_links(); ?>
+
+		<?php
+		if ( is_active_sidebar( 'footer' ) ) :
+			?>
+			<div class="footer-widgets widget-area">
+				<?php dynamic_sidebar( 'footer' ); ?>
+			</div>
+			
+			<?php
+		endif; ?>
 
 		<nav id="site-footer-navigation" class="footer-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Footer Navigation', 'debtcollective' ); ?>">
 			<?php
@@ -30,7 +41,22 @@
 
 		<div class="container site-info">
 			<?php debtcollective_display_copyright_text(); ?>
-			<?php debtcollective_display_social_network_links(); ?>
+
+			<nav id="site-privacy-navigation" class="privacy-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Privacy Navigation', 'debtcollective' ); ?>">
+				<?php
+				wp_nav_menu(
+					[
+						'fallback_cb'    => false,
+						'theme_location' => 'privacy',
+						'menu_id'        => 'privacy-menu',
+						'menu_class'     => 'menu container',
+						'container'      => false,
+						'depth'          => 1,
+					]
+				);
+				?>
+			</nav><!-- #site-navigation-->
+
 		</div><!-- .site-info -->
 
 	</footer><!-- .site-footer container-->
--- wp-content/themes/debtcollective/inc/setup.php
+++ wp-content/themes/debtcollective/inc/setup.php
@@ -48,9 +48,10 @@ function setup() {
 	// Register navigation menus.
 	register_nav_menus(
 		[
-			'primary' => esc_html__( 'Primary Menu', 'debtcollective' ),
-			'footer'  => esc_html__( 'Footer Menu', 'debtcollective' ),
-			'mobile'  => esc_html__( 'Mobile Menu', 'debtcollective' ),
+			'primary' 	=> esc_html__( 'Primary Menu', 'debtcollective' ),
+			'footer'  	=> esc_html__( 'Footer Menu', 'debtcollective' ),
+			'mobile'  	=> esc_html__( 'Mobile Menu', 'debtcollective' ),
+			'privacy'  	=> esc_html__( 'Privacy Menu', 'debtcollective' ),
 		]
 	);
 
@@ -100,31 +101,31 @@ function setup() {
 	add_theme_support( 'align-wide' );
 
 	// Gutenberg defaults for font sizes.
-	add_theme_support(
-		'editor-font-sizes',
-		[
-			[
-				'name' => __( 'Small', 'debtcollective' ),
-				'size' => 12,
-				'slug' => 'small',
-			],
-			[
-				'name' => __( 'Normal', 'debtcollective' ),
-				'size' => 16,
-				'slug' => 'normal',
-			],
-			[
-				'name' => __( 'Large', 'debtcollective' ),
-				'size' => 36,
-				'slug' => 'large',
-			],
-			[
-				'name' => __( 'Huge', 'debtcollective' ),
-				'size' => 50,
-				'slug' => 'huge',
-			],
-		]
-	);
+	// add_theme_support(
+	// 	'editor-font-sizes',
+	// 	[
+	// 		[
+	// 			'name' => __( 'Small', 'debtcollective' ),
+	// 			'size' => 12,
+	// 			'slug' => 'small',
+	// 		],
+	// 		[
+	// 			'name' => __( 'Normal', 'debtcollective' ),
+	// 			'size' => 16,
+	// 			'slug' => 'normal',
+	// 		],
+	// 		[
+	// 			'name' => __( 'Large', 'debtcollective' ),
+	// 			'size' => 36,
+	// 			'slug' => 'large',
+	// 		],
+	// 		[
+	// 			'name' => __( 'Huge', 'debtcollective' ),
+	// 			'size' => 50,
+	// 			'slug' => 'huge',
+	// 		],
+	// 	]
+	// );
 
 	// Gutenberg editor styles support.
 	add_theme_support( 'editor-styles' );
@@ -163,7 +164,7 @@ function widgets_init() {
 	// Define sidebars.
 	$sidebars = [
 		'sidebar-1' => esc_html__( 'Sidebar 1', 'debtcollective' ),
-		'footer' => esc_html__( 'Footer', 'debtcollective' ),
+		'footer' 	=> esc_html__( 'Footer', 'debtcollective' ),
 	];
 
 	// Loop through each sidebar and register.
