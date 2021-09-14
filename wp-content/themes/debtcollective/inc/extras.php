26103fb7e274d2b85d2ab0f4630bfb670c2051be
Pea <pea@misfist.com>
2021-09-13 20:06:14 -0400

    Add filters for contact 7 plugin

--- wp-content/themes/debtcollective/inc/extras.php
+++ wp-content/themes/debtcollective/inc/extras.php
@@ -28,3 +28,14 @@ function categorized_blog() {
 
 	return $category_count > 1;
 }
+
+/**
+ * Turn of autop for contact forms
+ * 
+ * @link https://contactform7.com/2018/01/31/contact-form-7-50/
+ */
+\add_filter( 'wpcf7_autop_or_not', '__return_false' );
+
+\add_filter( 'wpcf7_form_class_attr', function( $class ) {
+	return $class;
+} );
