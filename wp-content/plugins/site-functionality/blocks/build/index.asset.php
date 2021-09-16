8541c2d808708b504f2b9d36da3a9a68e94ccd76
Pea <pea@misfist.com>
2021-09-15 22:07:00 -0400

    Add FAQ component

--- wp-content/plugins/site-functionality/blocks/build/index.asset.php
+++ wp-content/plugins/site-functionality/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'db070db83408f2f9d054d394b233b0a0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '271daa0dd3ac95ad72a04afce874c489');
