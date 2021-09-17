59a1abbd0f3ae66418898d85944dc312a3c55a68
robu9 <robusprout@gmail.com>
2021-09-17 01:34:27 -0400

    footer styling in progress, turn off no important rule in linter config

--- wp-content/themes/debtcollective/build/index.asset.php
+++ wp-content/themes/debtcollective/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '46a826cce22c41095e98bacd1cfb7821');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'edb13a227651ce5786feeb491edfb440');
