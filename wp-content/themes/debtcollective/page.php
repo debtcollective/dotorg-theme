1ca9bdcdb94fdb1630b361b0e06d7c1cd3300e9d
Pea <pea@misfist.com>
2021-09-22 21:03:06 -0400

    Hide sidebar on home

--- wp-content/themes/debtcollective/page.php
+++ wp-content/themes/debtcollective/page.php
@@ -23,7 +23,9 @@ get_header(); ?>
 
 		</main><!-- #main -->
 
-		<?php get_sidebar(); ?>
+		<?php if( !is_front_page() ) : ?>
+			<?php get_sidebar(); ?>
+		<?php endif; ?>
 	</div>
 
 <?php get_footer(); ?>
