3ea00ebe585c2032c035dfeff8235a886786dce6
Pea <pea@misfist.com>
2021-10-01 16:19:38 -0400

    Remove styles for social links

---
+++ wp-content/themes/debtcollective/src/js/blocks/filtered/social-links.js
@@ -0,0 +1,23 @@
+import lodash from 'lodash';
+
+/**
+ * Modify settings for core/social-links
+ * @param {Object} settings 
+ * @param {String} name 
+ * @return {Object}
+ */
+function modifySocialLinkSettings( settings, name ) {
+    if ( name !== 'core/social-links' ) {
+        return settings;
+    }
+ 
+    return lodash.assign( {}, settings, {
+        style: lodash.assign( {}, settings.style, '' ),
+    } );
+}
+ 
+wp.hooks.addFilter(
+    'blocks.registerBlockType',
+    'debtcollective/social-links',
+    modifySocialLinkSettings
+);
