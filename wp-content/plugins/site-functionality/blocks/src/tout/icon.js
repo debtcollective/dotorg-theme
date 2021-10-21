1a15fc92ad938b69621744dc7abea8dfb747c1d9
Pea <pea@misfist.com>
2021-10-20 20:52:36 -0400

    Add touts block

---
+++ wp-content/plugins/site-functionality/blocks/src/tout/icon.js
@@ -0,0 +1,14 @@
+
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const icon = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M0 0h24v24H0V0z" fill="none"/>
+		<Path d="M4 15h16v-2H4v2zm0 4h16v-2H4v2zm0-8h16V9H4v2zm0-6v2h16V5H4z"/>
+	</SVG>
+    );
+
+export default icon;
---
+++ wp-content/plugins/site-functionality/blocks/src/tout/save.js
@@ -0,0 +1,12 @@
+import { 
+    InnerBlocks, 
+    useBlockProps
+} from '@wordpress/block-editor';
+
+const Save = ( props ) => {
+
+	return <InnerBlocks.Content />;
+    // return null;
+};
+
+export default Save;
