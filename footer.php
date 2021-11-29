e6fd75ef41586932ec77d92ff35b692e303eb1c5
Thiago de Mello Bueno <thiago@debtcollective.org>
2021-11-29 09:43:24 -0500

    Some footer tweaks
    including max width centering

--- footer.php
+++ footer.php
@@ -12,53 +12,55 @@
 ?>
 
 	<footer class="site-footer">
-		<?php debtcollective_display_social_network_links(); ?>
+		<div class="container">
+			<?php debtcollective_display_social_network_links(); ?>
 
-		<?php
-		if ( is_active_sidebar( 'footer' ) ) :
-			?>
-			<div class="footer-widgets widget-area">
-				<?php dynamic_sidebar( 'footer' ); ?>
-			</div>
-			
 			<?php
-		endif;
-		?>
+			if ( is_active_sidebar( 'footer' ) ) :
+				?>
+				<div class="footer-widgets widget-area">
+					<?php dynamic_sidebar( 'footer' ); ?>
+				</div>
 
-		<nav id="site-footer-navigation" class="footer-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Footer Navigation', 'debtcollective' ); ?>">
-			<?php
-			wp_nav_menu(
-				[
-					'fallback_cb'    => false,
-					'theme_location' => 'footer',
-					'menu_id'        => 'footer-menu',
-					'menu_class'     => 'menu container',
-					'container'      => false,
-					'depth'          => 2,
-				]
-			);
+				<?php
+			endif;
 			?>
-		</nav><!-- #site-navigation-->
 
-		<div class="container site-info">
-			<?php debtcollective_display_copyright_text(); ?>
-
-			<nav id="site-privacy-navigation" class="privacy-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Privacy Navigation', 'debtcollective' ); ?>">
+			<nav id="site-footer-navigation" class="footer-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Footer Navigation', 'debtcollective' ); ?>">
 				<?php
 				wp_nav_menu(
 					[
 						'fallback_cb'    => false,
-						'theme_location' => 'privacy',
-						'menu_id'        => 'privacy-menu',
+						'theme_location' => 'footer',
+						'menu_id'        => 'footer-menu',
 						'menu_class'     => 'menu container',
 						'container'      => false,
-						'depth'          => 1,
+						'depth'          => 2,
 					]
 				);
 				?>
 			</nav><!-- #site-navigation-->
 
-		</div><!-- .site-info -->
+			<div class="container site-info">
+				<?php debtcollective_display_copyright_text(); ?>
+
+				<nav id="site-privacy-navigation" class="privacy-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Privacy Navigation', 'debtcollective' ); ?>">
+					<?php
+					wp_nav_menu(
+						[
+							'fallback_cb'    => false,
+							'theme_location' => 'privacy',
+							'menu_id'        => 'privacy-menu',
+							'menu_class'     => 'menu container',
+							'container'      => false,
+							'depth'          => 1,
+						]
+					);
+					?>
+				</nav><!-- #site-navigation-->
+
+			</div><!-- .site-info -->
+		</div><!-- .container -->
 
 	</footer><!-- .site-footer container-->
 
