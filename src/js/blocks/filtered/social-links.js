687e7731291b5d83bb77b5045182cad2d4546a5d
Thiago de Mello Bueno <thiago@debtcollective.org>
2021-12-16 17:19:25 -0500

    some padding and max widths in the editor interface

--- src/js/blocks/filtered/social-links.js
+++ src/js/blocks/filtered/social-links.js
@@ -2,19 +2,19 @@ import lodash from 'lodash';
 
 /**
  * Modify settings for core/social-links
- * @param {Object} settings 
- * @param {String} name 
+ * @param {Object} settings
+ * @param {String} name
  * @return {Object}
  */
-function modifySocialLinkSettings( settings, name ) {
-    if ( name !== 'core/social-links' ) {
-        return settings;
-    }
- 
-    return lodash.assign( {}, settings, {
-        style: lodash.assign( {}, settings.style, '' ),
-    } );
-}
+  function modifySocialLinkSettings( settings, name ) {
+      if ( name !== 'core/social-links' ) {
+          return settings;
+      }
+
+      return lodash.assign( {}, settings, {
+          style: lodash.assign( {}, settings.style, '' ),
+      } );
+  }
  
 wp.hooks.addFilter(
     'blocks.registerBlockType',
