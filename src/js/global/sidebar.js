b02e67dd6b1389eb233c5a054f5f895642717ca0
Thiago de Mello Bueno <thiago@debtcollective.org>
2021-12-20 17:34:59 -0500

    various tweaks to layout

--- src/js/global/sidebar.js
+++ src/js/global/sidebar.js
@@ -1,9 +1,9 @@
-const Sticky = require('sticky-js');
-
-const sidebar = document.querySelector('.sidebar');
-if (sidebar) {
-	const sticky = new Sticky('.sidebar');
-	sidebar.setAttribute('data-sticky-for', '1080');
-	sidebar.setAttribute('data-margin-top', '128');
-	sidebar.setAttribute('data-margin-bottom', '64');
-}
+// const Sticky = require('sticky-js');
+// as we add things to the sidebar, they become inaccessible when the sidebar is taller than screen width, so for now, killing the sticky sidebar
+// const sidebar = document.querySelector('.sidebar');
+// if (sidebar) {
+// 	const sticky = new Sticky('.sidebar');
+// 	sidebar.setAttribute('data-sticky-for', '1080');
+// 	sidebar.setAttribute('data-margin-top', '128');
+// 	sidebar.setAttribute('data-margin-bottom', '64');
+// }
