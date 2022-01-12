27e214c0acfa9f58d49d5c1e570dda81a72d92be
Thiago de Mello Bueno <thiago@debtcollective.org>
2022-01-12 09:19:25 -0500

    fix matchheight for new tout arrangements

--- src/js/toutmatchheight.js
+++ src/js/toutmatchheight.js
@@ -1,11 +1,11 @@
 import MatchHeight from '@tannerhodges/match-height'
-
 if( window ) {
   window.addEventListener('DOMContentLoaded', function() {
     // touts really should only live in columns (and if they arent, they shouldnt matchheight with anything)
-    const cols = document.querySelectorAll('.wp-block-columns')
-    cols.forEach( function(col,i){
-      const touts = col.querySelectorAll('.tout')
+    const groups = document.querySelectorAll('.wp-block-group')
+    groups.forEach( function(grp,i){
+      const touts = grp.querySelectorAll('.tout')
+      if (touts.length < 2) return;
       touts.forEach( function(tout) {
         // group titles and contents in a wrapper here to get easier less awkward layouts.
         const title = tout.querySelector('.tout__title')
@@ -30,15 +30,17 @@ if( window ) {
 }
 
 function toutMatchHeight() {
-  console.log('toutMatchHeight')
-  const cols = document.querySelectorAll('.wp-block-columns')
-  cols.forEach( function(col){
-    const touts = col.querySelectorAll('.tout')
-    const wrapper = col.querySelectorAll('.content__wrapper')
-    const content = col.querySelectorAll('.tout__content')
+  const groups = document.querySelectorAll('.wp-block-group')
+  groups.forEach( function(grp,i){
+    const touts = grp.querySelectorAll('.tout')
+    if (touts.length < 2) return;
+    const wrapper = grp.querySelectorAll('.content__wrapper')
+    const title = grp.querySelectorAll('.tout__title')
+    const content = grp.querySelectorAll('.tout__content')
     if (touts.length > 1) {
       MatchHeight.add(touts)
       MatchHeight.add(wrapper)
+      MatchHeight.add(title)
       MatchHeight.add(content)
     }
   })
