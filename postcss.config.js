a173cbc174e3e6baaf787480d40bca9495b109ad
robu9 <robusprout@gmail.com>
2022-03-04 14:07:05 -0500

    add postcss-aspect-ratio-polyfill plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-	plugins: {
-		'postcss-preset-env': {
-			autoprefixer: {
-				flexbox: 'no-2009',
-			},
-			stage: 3,
-			features: {
-				'nesting-rules': true,
+	plugins: [
+		'postcss-aspect-ratio-polyfill',
+		{
+			'postcss-preset-env': {
+				autoprefixer: {
+					flexbox: 'no-2009',
+				},
+				stage: 3,
+				features: {
+					'nesting-rules': true,
+				},
 			},
 		},
-	},
+	],
 };
