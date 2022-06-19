da90b54f287a9daca6b383296caa573e1ce929eb
Pea <pea@misfist.com>
2022-06-18 20:34:14 -0400

    Add template tag new `#_RECURRENCES` placeholder

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -843,6 +843,36 @@ function debtcollective_virtual_location( $EM_Event, $args = array() ) {
 		endif;
 	}
 }
+
+/**
+ * Render Recurrences
+ *
+ * @param int   $recurrence_id
+ * @param array $args
+ * @param int   $current_post_id
+ * @return void
+ */
+function debtcollective_event_recurrences_placeholders( $EM_Event, $args = array() ) {
+	if ( ! class_exists( '\EM_Events' ) ) {
+		return;
+	}
+	$defaults = array(
+		'title'         => esc_html__( 'Events in Series', 'debt-collective' ),
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+	?>
+
+	<section class="recurrences">
+		<h4 class="recurrences__title"><?php echo $args['title']; ?></h4>
+		<?php
+		echo $EM_Event->output( '#_RECURRENCES' );
+		?>
+	</section>
+		
+	<?php
+}
+
 /**
  * Render Event Map
  *
