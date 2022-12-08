2286d6a726a1cfd08ae0e9ac354d427d293cd11d
Pea <pea@misfist.com>
2022-12-07 22:13:16 -0500

    WP coding standards

--- header.php
+++ header.php
@@ -37,10 +37,10 @@ $display_site_branding = get_theme_mod( 'display_site_branding', 'true' );
 		<dc-header
 			logo=<?php echo esc_url( \get_stylesheet_directory_uri() . '/build/images/logo-black.png' ); ?>
 			logosmall=<?php echo esc_url( \get_stylesheet_directory_uri() . '/build/images/logo-small.png' ); ?>
-			community="<?php echo getenv('COMMUNITY_URL'); ?>"
+			community="<?php echo getenv( 'COMMUNITY_URL' ); ?>"
 			homepage="/"
-			wordpress="<?php echo getenv('WORDPRESS_URL'); ?>"
-			returnurl="<?php echo getenv('RETURN_URL'); ?>"
+			WordPress="<?php echo getenv( 'WORDPRESS_URL' ); ?>"
+			returnurl="<?php echo getenv( 'RETURN_URL' ); ?>"
 			id="dc-header"
 		></dc-header>
 	<?php endif; ?>
