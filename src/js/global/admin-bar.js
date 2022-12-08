2464ae8992bbe79513c6a099dc5ea536636223be
Pea <pea@misfist.com>
2022-12-08 18:02:13 -0500

    Add conditional for admin menu

--- src/js/global/admin-bar.js
+++ src/js/global/admin-bar.js
@@ -4,18 +4,24 @@ document.addEventListener('DOMContentLoaded', function () {
 	// create <style> outside of function scope so that it gets updated when called onresize
 	const style = document.createElement('style');
 
-	const addTopMargin = function () {
-		if (wpadminbar) {
-			const adminbarHeight = wpadminbar.offsetHeight;
-			style.innerText = `:host(dc-header) .navbar-top {margin-top:${adminbarHeight}px;}`;
-			// return if style tag is already present, as it has been updated at this point
-			if (dcheader[0].shadowRoot.lastChild && dcheader[0].shadowRoot.lastChild.tagName === 'STYLE') {
-				return
+	if( dcheader ) {
+		const addTopMargin = function () {
+			if ( !dcheader.length ) {
+				return;
 			}
-			dcheader[0].shadowRoot.appendChild(style);
-		}
-	};
+			if (wpadminbar) {
+				const adminbarHeight = wpadminbar.offsetHeight;
+				style.innerText = `:host(dc-header) .navbar-top {margin-top:${adminbarHeight}px;}`;
+				// return if style tag is already present, as it has been updated at this point
+				if (dcheader[0].shadowRoot.lastChild && dcheader[0].shadowRoot.lastChild.tagName === 'STYLE') {
+					return
+				}
+				dcheader[0].shadowRoot.appendChild(style);
+			}
+		};
+	
+		addTopMargin();
+		window.onresize = addTopMargin;
+	}
 
-	addTopMargin();
-	window.onresize = addTopMargin;
 });
