2d8bd2ded7e053996873d83d2eab01f8f9332910
Pea <pea@misfist.com>
2022-12-13 20:07:02 -0500

    Remove past event section

--- plugins/events-manager/templates/events-list.php
+++ plugins/events-manager/templates/events-list.php
@@ -8,61 +8,23 @@
  * $args - the args passed onto EM_Events::output()
  *
  */
-$upcoming_defaults = apply_filters( 'em_content_events_args', $args );
-$page =  isset( $_GET['pno'] ) ? (int) $_GET['pno'] : get_query_var( 'pno', 1 );
-
-$scope = 'future';
-$sort  = 'ASC';
+$defaults = apply_filters( 'em_content_events_args', $args );
+$page     = isset( $_GET['pno'] ) ? (int) $_GET['pno'] : get_query_var( 'pno', 1 );
 
 $outcoming_args = array(
-	'scope'         => $scope,
-	'order'         => $sort,
-	'limit'         => 0,
-	'pagination'    => 0,
+	'scope'      => get_option( 'dbem_events_page_scope', 'future' ),
+	'limit'      => get_option( 'dbem_events_default_limit', 24 ),
+	'order'      => get_option( 'dbem_events_default_order', 'ASC' ),
+	'orderby'    => get_option( 'dbem_events_default_orderby', 'event_start_date,event_start_time,event_name' ),
+	'pagination' => 1,
 );
 
-$outcoming_args = wp_parse_args( $outcoming_args, $upcoming_defaults ); ?>
+$outcoming_args = wp_parse_args( $outcoming_args, $defaults ); ?>
 
 <section class="events_list <?php echo esc_attr( $scope ); ?>">
 	
 	<?php
-	if( 1 === $page ) :
-		?>
-		<header class="events__header">
-			<h2 class="events__title"><?php esc_html_e( 'Upcoming', 'debtcollective' ); ?></h2>
-		</header>
-
-		<?php
-		echo EM_Events::output( $outcoming_args );
-	endif;
+	echo EM_Events::output( $outcoming_args );
 	?>
 	
 </section><!-- .events.past -->
-
-<?php
-$past_defaults = apply_filters( 'em_content_events_args', $args );
-
-$scope = 'past';
-$sort  = 'DESC';
-
-$past_args = array(
-	'scope'         => $scope,
-	'order'         => $sort,
-	'limit'         => get_option( 'dbem_events_default_limit', 24 ),
-	'pagination'    => 1,
-);
-
-$past_args = wp_parse_args( $past_args, $past_defaults );
-?>
-
-<section class="events past">
-
-	<header class="events__header">
-		<h2 class="events__title"><?php esc_html_e( 'Past', 'debtcollective' ); ?></h2>
-	</header>
-
-	<?php
-	echo EM_Events::output( $past_args );
-	?>
-	
-</section><!-- .events.upcoming -->
