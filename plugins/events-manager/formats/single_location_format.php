8355bf730d402a97671ee7857533fd52eaf17056
Pea <pea@misfist.com>
2023-01-16 22:18:00 -0500

    Add event and location format

---
+++ plugins/events-manager/formats/single_location_format.php
@@ -0,0 +1,36 @@
+<article id="post-#_LOCATIONPOSTID" class="event-container event-location">
+
+	{has_loc_image}
+	<div class="event__image wp-block-cover">
+		#_LOCATIONIMAGE{full}
+	</div>
+	{/has_loc_image}
+	
+	<header class="event_header">
+		<h2 class="event__title">#_LOCATIONNAME</h2>
+	</header>
+
+	<div class="event__meta post-meta">
+		<div class="event__location">
+			<div class="event__location-address">
+				<div class="event__address">
+					<div class="event__location-street">#_LOCATIONADDRESS</div>
+					<div class="event__location-city">#_LOCATIONTOWN</div>
+					<div class="event__location-state">#_LOCATIONSTATE</div>
+					<div class="event__location-zip">#_LOCATIONPOSTCODE</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="event__content">
+		#_LOCATIONNOTES
+
+		<div class="event__map">
+			#_LOCATIONMAP
+		</div>
+	</div>
+
+	<footer class="event__footer"></footer>
+
+</article><!-- #post-## -->
