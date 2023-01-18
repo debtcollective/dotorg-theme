8bc084d1a41b37d43f6c92239474b8cf0e911bec
Pea <pea@misfist.com>
2023-01-18 18:24:09 -0500

    Add recurrences to single event format

--- plugins/events-manager/formats/single_event_format.php
+++ plugins/events-manager/formats/single_event_format.php
@@ -97,6 +97,13 @@
 	<footer class="event__footer">
 		<div class="post-meta categories">#_EVENTCATEGORIES</div>
 		<div class="post-meta tags">#_EVENTTAGS</div>
+
+		{is_recurring}
+		<section class="recurrences">
+			<h3 class="recurrences__title"><?php esc_html_e( 'Events in Series', 'debtcollective' ); ?></h3>
+			#_RECURRENCES
+		</section>
+		{/is_recurring}
 	</footer>
 
 </article><!-- #post-## -->
