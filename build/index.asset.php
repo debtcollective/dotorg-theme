7427a876851656277db55bd8acbe0260ef0ff10b
robosprout <robosprout9@gmail.com>
2024-07-12 17:27:06 -0400

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '261f9ca9fa35662eaedc0dbca05e49a3');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f2ec614f8b2727243eff0c909cdb5888');
